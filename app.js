1ba71305396c08d627e084af9ba605ba450eff0f
HakimSara <hakimsara145@gmail.com>
2024-04-16 20:33:03 -0300

    CRUD FIRESTORE

--- app.js
+++ app.js
@@ -1,11 +1,50 @@
- db.collection('libri-firestore')
-    .get()
-    .then(
-        (snapshot)=>{
-           // console.log(snapshot.docs)
-           snapshot.docs.forEach(doc => {
-            console.log(doc.data());
-           });
-        }
-    ) 
-       
+const livroList= document.querySelector('#book-list');
+
+function renderBook(doc){
+
+   //criação dos elementos html
+   let li= document.createElement('li');
+   let titulo= document.createElement('span');
+   let autor = document.createElement('span');
+
+   //carrega os dados dos elementos html
+   li.setAttribute('data-id', doc.id);
+   titulo.textContent= doc.data().titulo;
+   autor.textContent= doc.data().autor;
+
+   //adiciona os dados de autor e titulo na tag li;
+   li.appendChild(titulo); 
+   li.appendChild(autor);
+   //adiciona o li na tag ul
+   livroList.appendChild(li);
+
+}
+
+db.collection('libri-firestore')
+.get()
+.then(
+    (snapshot)=>{
+       // console.log(snapshot.docs)
+       snapshot.docs.forEach(doc => {
+        console.log(doc.data());
+        renderBook(doc);
+       });
+    }
+);
+
+const form = document.querySelector('#add-book-form');
+
+form.addEventListener('submit', (event)=>{
+
+   event.preventDefault();
+   
+   db.collection('libri-firestore').add({
+      autor: form.autor.value,
+      titulo: form.titulo.value
+   }).then(()=>{
+      form.autor.value= '';
+      form.titulo.value='';
+      window.location.reload();
+   });
+   
+});
